0aee90e0ea7711f1ac7eaac6cb81e7bf778aaa87
yaohuanhuan <1107129170@qq.com>
2016-08-15 21:07:34 +0800

    初步抽离FreshNewsAdapter

---
+++ app/src/main/java/com/yx/jiandan/adapter/FreshNewsAdapter.java
@@ -0,0 +1,121 @@
+package com.yx.jiandan.adapter;
+
+import android.app.Activity;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.yx.jiandan.R;
+import com.yx.jiandan.bean.FreshNews;
+import com.yx.jiandan.bean.Posts;
+import com.yx.jiandan.ui.imageload.ImageLoadProxy;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Created by Y on 2016/8/15.
+ */
+public class FreshNewsAdapter extends RecyclerView.Adapter<FreshNewsAdapter.FreshNewsViewHolder> {
+
+    public static final String URL_FRESH_NEWS = "http://jandan.net/?oxwlxojflwblxbsapi=get_recent_posts&include=url," +
+            "date,tags,author,title,comment_count,custom_fields&custom_fields=thumb_c,views&dev=1&page=1";
+
+    private ArrayList<Posts> mPost;
+    private DisplayImageOptions options;
+
+    Activity activity;
+
+    public FreshNewsAdapter(Activity activity) {
+        this.activity = activity;
+        mPost = new ArrayList<>();
+    }
+
+    @Override
+    public FreshNewsAdapter.FreshNewsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_fresh_news_small, parent, false);
+        FreshNewsAdapter.FreshNewsViewHolder holder = new FreshNewsAdapter.FreshNewsViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(FreshNewsViewHolder holder, int position) {
+        holder.tv_title.setText(mPost.get(position).getTitle());
+        holder.tv_name.setText(mPost.get(position).getId() + "");
+        holder.tv_date.setText(mPost.get(position).getDate());
+        options = ImageLoadProxy.getOptions4PictureList(R.mipmap.ic_loading_small);
+        ImageLoadProxy.displayImage(mPost.get(position).getThumb_c(), holder.iv_pic, options);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPost.size();
+    }
+
+    public void loadDate() {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(URL_FRESH_NEWS).build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String s = response.body().string();
+                try {
+                    Log.e("test", "数据刷新啦！");
+                    JSONObject object = new JSONObject(s);
+                    FreshNews freshNews = new FreshNews(object);
+                    JSONArray array = freshNews.getPosts();
+                    for (int i = 0; i < array.length(); i++) {
+                        Posts post = new Posts(array.getJSONObject(i));
+                        mPost.add(post);
+                    }
+                    activity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            notifyDataSetChanged();
+                        }
+                    });
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public static class FreshNewsViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tv_title;
+        TextView tv_name;
+        TextView tv_date;
+        ImageView iv_pic;
+
+        public FreshNewsViewHolder(View itemView) {
+            super(itemView);
+            tv_title = (TextView) itemView.findViewById(R.id.tv_title);
+            tv_name = (TextView) itemView.findViewById(R.id.tv_name);
+            tv_date = (TextView) itemView.findViewById(R.id.tv_date);
+            iv_pic = (ImageView) itemView.findViewById(R.id.iv_pic);
+        }
+    }
+
+}
--- app/src/main/java/com/yx/jiandan/ui/fragment/FreshNewsFragment.java
+++ app/src/main/java/com/yx/jiandan/ui/fragment/FreshNewsFragment.java
@@ -7,46 +7,23 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache;
-import com.nostra13.universalimageloader.core.DisplayImageOptions;
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
 import com.yx.jiandan.R;
-import com.yx.jiandan.bean.FreshNews;
+import com.yx.jiandan.adapter.FreshNewsAdapter;
 import com.yx.jiandan.bean.Posts;
 import com.yx.jiandan.callback.LoadFinishCallBack;
 import com.yx.jiandan.callback.LoadMoreListener;
-import com.yx.jiandan.ui.imageload.ImageLoadProxy;
 import com.yx.jiandan.view.AutoLoadRecyclerView;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-
 
 public class FreshNewsFragment extends Fragment {
-
-    public static final String URL_FRESH_NEWS = "http://jandan.net/?oxwlxojflwblxbsapi=get_recent_posts&include=url," +
-            "date,tags,author,title,comment_count,custom_fields&custom_fields=thumb_c,views&dev=1&page=1";
 
     private String TAG = "FresNews";
 
@@ -54,7 +31,6 @@ public class FreshNewsFragment extends Fragment {
     private SwipeRefreshLayout swipeRefreshLayout;
     private List<Posts> list;
     private FreshNewsAdapter freshNewsAdapter;
-    private DisplayImageOptions options;
     private LoadFinishCallBack mLoadFinisCallBack;
 
     @Override
@@ -66,21 +42,31 @@ public class FreshNewsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_fresh_news, container, false);
+        return view;
+    }
 
-        recyclerView = (AutoLoadRecyclerView) view.findViewById(R.id.recycler_view);
-        swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipeRefreshLayout);
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        recyclerView = (AutoLoadRecyclerView) getView().findViewById(R.id.recycler_view);
+        swipeRefreshLayout = (SwipeRefreshLayout) getView().findViewById(R.id.swipeRefreshLayout);
         swipeRefreshLayout.setColorSchemeResources(android.R.color.holo_blue_bright,
                 android.R.color.holo_green_light,
                 android.R.color.holo_orange_light,
                 android.R.color.holo_red_light);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setOnPauseListenerParams(false, true);
+        freshNewsAdapter = new FreshNewsAdapter(getActivity());
+        recyclerView.setAdapter(freshNewsAdapter);
+        freshNewsAdapter.loadDate();
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
+                freshNewsAdapter.loadDate();
                 swipeRefreshLayout.setRefreshing(false);
             }
         });
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setOnPauseListenerParams(false, true);
+
         mLoadFinisCallBack = recyclerView;
 
         recyclerView.setLoadMoreListener(new LoadMoreListener() {
@@ -89,86 +75,6 @@ public class FreshNewsFragment extends Fragment {
                 Log.e(TAG, "loadMore");
             }
         });
-        freshNewsAdapter = new FreshNewsAdapter();
-        recyclerView.setAdapter(freshNewsAdapter);
-        return view;
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder().url(URL_FRESH_NEWS).build();
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                mLoadFinisCallBack.loadFinish(null);
-                String s = response.body().string();
-                try {
-                    JSONObject object = new JSONObject(s);
-                    FreshNews freshNews = new FreshNews(object);
-                    JSONArray array = freshNews.getPosts();
-                    for (int i = 0; i < array.length(); i++) {
-                        Posts post = new Posts(array.getJSONObject(i));
-                        list.add(post);
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                getActivity().runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        freshNewsAdapter.notifyDataSetChanged();
-                    }
-                });
-
-            }
-        });
-    }
-
-    public class FreshNewsAdapter extends RecyclerView.Adapter<FreshNewsViewHolder> {
-
-        @Override
-        public FreshNewsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            FreshNewsViewHolder holder = new FreshNewsViewHolder(LayoutInflater.from(
-                    getContext()).inflate(R.layout.item_fresh_news_small, parent, false));
-            return holder;
-        }
-
-        @Override
-        public void onBindViewHolder(FreshNewsViewHolder holder, int position) {
-            holder.tv_title.setText(list.get(position).getTitle());
-            holder.tv_name.setText(list.get(position).getId() + "");
-            holder.tv_date.setText(list.get(position).getDate());
-            options = ImageLoadProxy.getOptions4PictureList(R.mipmap.ic_loading_small);
-            ImageLoadProxy.displayImage(list.get(position).getThumb_c(), holder.iv_pic, options);
-        }
-
-        @Override
-        public int getItemCount() {
-            return list.size();
-        }
-    }
-
-    public static class FreshNewsViewHolder extends RecyclerView.ViewHolder {
-
-        TextView tv_title;
-        TextView tv_name;
-        TextView tv_date;
-        ImageView iv_pic;
-
-
-        public FreshNewsViewHolder(View itemView) {
-            super(itemView);
-            tv_title = (TextView) itemView.findViewById(R.id.tv_title);
-            tv_name = (TextView) itemView.findViewById(R.id.tv_name);
-            tv_date = (TextView) itemView.findViewById(R.id.tv_date);
-            iv_pic = (ImageView) itemView.findViewById(R.id.iv_pic);
-        }
-    }
 }
